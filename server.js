93a9746b28bfb43fed9004d061f8cfb2e2ec8044
Shaik Lal John Basha <your-github-email@gmail.com>
2026-03-04 02:06:23 +0530

    Added routes and models folders

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
+// 👇 ADD THIS LINE HERE
+app.use("/api/auth", require("./routes/authRoutes"));
+
 app.get("/", (req, res) => {
   res.send("Auth System API Running...");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
